2c5de6516af338c3a54810f5e1355b5b8d3b60a9
dd-mooon <vjwyp0308@gmail.com>
2026-01-14 18:17:56 +0900

    feat: add player section

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,18 @@ function App() {
   
 
   return (
-    <p>Coming soon</p>
+    <div id="game-container">
+      <ol id="player">
+        <li>
+          <span className="player-name">Player 1</span>
+          <span className="player-symbol">X</span>
+        </li>
+        <li>
+          <span className="player-name">Player 2</span>
+          <span className="player-symbol">O</span>
+        </li>       
+      </ol>
+    </div>
   )
 }
 
