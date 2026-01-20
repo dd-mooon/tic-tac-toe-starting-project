ce84f28838fe392f901859482373447800e2bb4b
dd-mooon <vjwyp0308@gmail.com>
2026-01-20 18:56:01 +0900

    feat: add player component

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,11 @@
+import Player from "./components/Player.jsx";
 
 function App() {
-  
-
   return (
     <div id="game-container">
-      <ol id="player">
-        <li>
-          <span className="player-name">Player 1</span>
-          <span className="player-symbol">X</span>
-        </li>
-        <li>
-          <span className="player-name">Player 2</span>
-          <span className="player-symbol">O</span>
-        </li>       
+      <ol id="players">
+      <Player name ="payler 1" symbol ="X" />
+      <Player name ="payler 2" symbol ="O" />
       </ol>
     </div>
   )
